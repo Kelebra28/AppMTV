b643fa327155555131ab83107752248d30562ebc
Ricardo <ricardokelebra@gmail.com>
2019-04-24 10:19:34 -0500

    Add Phone Number

--- server/models/user.js
+++ server/models/user.js
@@ -21,6 +21,10 @@ module.exports = (sequelize, DataTypes) => {
         type:DataTypes.STRING,
         allowNull:false
       },
+      phoneNumber:{
+        type:DataTypes.STRING,
+        allowNull:false
+      },
     email: {
       type:DataTypes.STRING,
       allowNull:false,
